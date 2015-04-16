a7537db52bcbfe1933c4daa3dd286b2ad46c11f6
unknown <nicolasferec@outlook.com>
2015-04-16 03:15:15 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 
 	$headers = 	'From: ' . $_POST['email'] . "\r\n" .
 			    'X-Mailer: PHP/' . phpversion();
-	$message = htmlspecialchars($_POST['message'])
+	$message = htmlspecialchars($_POST['message']);
 
 	mail('nicolas@ferec.me', 'Mail from ferec.me contact form', $message, $headers);
 
